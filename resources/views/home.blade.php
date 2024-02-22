019f9e21950169af211166165d90be2c14b94424
pakria <fakrialfarizi@gmail.com>
2024-02-22 18:17:47 +0700

    responsive design for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,161 +1,164 @@
 @extends('layouts.app')
 @section('content')
     <div class="w-full">
-        <div id="hero" class="w-full h-fit px-14 flex bg-seconder bg-cover bg-center bg-no-repeat">
-            <div class="w-2/3 my-8 py-8">
-                <h1 class="text-6xl font-bold">Spark your imagination, fuel your mind with wonderful world.</h1>
-                <h6 class="text-md font-semibold my-6">Explore the place to enjoy the world of reading with an interesting
-                    experience</h6>
-                <a class=" border-2 border-white hover:border-primary font-semibold py-2 px-8 rounded-xl"
-                    href="/register">Sign Up<i class="fa-solid fa-arrow-right"></i>
+        <div id="hero"
+            class="w-full h-fit flex items-center bg-seconder bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-3 md:py-6 lg:py-8 text-xs md:text-lg lg:text-2xl">
+            <div class="w-2/3 my-3 md:my-6 lg:my-8">
+                <h1 class="font-bold">Spark your imagination, fuel your mind with wonderful world.</h1>
+                <h6 class="font-semibold my-4 md:my-6 lg:my-8">Explore the place to enjoy the world of reading with an
+                    interesting experience</h6>
+                <a class=" border-2 border-white hover:border-primary text-xs md:text-lg lg:text-2xl font-semibold py-1 md:py-2 px-4 md:px-6 lg:px-8 rounded-md md:rounded-xl"
+                    href="/register">Sign Up
                 </a>
             </div>
-            <div class="w-1/3 py-12">
-                <img src="{{ asset('images/hero.png') }}" class="w-max" alt="">
-            </div>
-        </div>
-        <div id="feature" class="w-full h-fit px-14 py-12 bg-white text-black bg-cover bg-center bg-no-repeat">
-            <div class="grid justify-center items-center text-center">
-                <h2 class="text-3xl font-semibold">Features</h2>
-                <h6>We have advantages over other digital libraries</h6>
-            </div>
-            <div class="flex mx-12 my-12 gap-24">
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12 px-3">
-                        <div class="flex bg-seconder p-12 justify-center m-auto items-center rounded-2xl">
-                            <img src="{{ asset('images/collection.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4 text-center">
-                            <h3 class="text-2xl font-bold">Collection Book</h3>
-                            <h6>You can search book in any other genres or other categories</h6>
-                        </div>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12 px-3">
-                        <div class="flex bg-seconder p-12 justify-center m-auto items-center rounded-2xl">
-                            <img src="{{ asset('images/search.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4">
-                            <h3 class="text-2xl font-bold">Search Book</h3>
-                            <h6>You can search book in any other genres or other categories</h6>
-                        </div>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12 px-3">
-                        <div class="flex bg-seconder p-12 justify-center m-auto items-center rounded-2xl">
-                            <img src="{{ asset('images/chat.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4">
-                            <h3 class="text-2xl font-bold">Review Book</h3>
-                            <h6>You can search book in any other genres or other categories</h6>
-                        </div>
-                    </div>
-                </a>
+            <div class="w-1/3">
+                <img src="{{ asset('images/hero.png') }}" class="w-28 md:w-44 lg:w-60" alt="">
             </div>
         </div>
-        <div id="book" class="w-full h-fit px-14 py-12 bg-white text-black bg-cover bg-center bg-no-repeat">
+        <div id="feature"
+            class="w-full h-fit bg-white text-black bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
             <div class="grid justify-center items-center text-center">
-                <h2 class="text-3xl font-semibold">Categories</h2>
-                <h6>We have advantages over other digital libraries</h6>
+                <h2 class="text-lg md:text-3xl font-bold">Features</h2>
+                <h6 class="text-sm md:text-xl lg:text-2xl">We have advantages over other digital libraries</h6>
             </div>
-            <div class="flex mx-12 my-12 gap-24">
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12">
-                        <div class="flex p-8 justify-center mx-20 py-12 items-center rounded-2xl">
-                            <img src="{{ asset('images/comic.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4 text-center">
-                            <h3 class="text-2xl font-bold">Comics</h3>
-                        </div>
+            <div class="grid grid-cols-3 gap-4 md:gap-8 mt-4 md:mt-12">
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="grid bg-seconder items-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/collection.png') }}" class="w-8 md:w-20 lg:w-24" alt="logo">
                     </div>
-                </a>
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12">
-                        <div class="flex p-8 justify-center mx-20 py-12 items-center rounded-2xl">
-                            <img src="{{ asset('images/novel.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4">
-                            <h3 class="text-2xl font-bold">Novels</h3>
-                        </div>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Collection Book</h3>
+                        <h6>You can search book in any other genres or other categories</h6>
                     </div>
-                </a>
-                <a href="#">
-                    <div class="grid rounded-3xl shadow-md justify-center items-center text-center py-12">
-                        <div class="flex p-8 justify-center mx-20 py-12 items-center rounded-2xl">
-                            <img src="{{ asset('images/edu.png') }}" class="w-30" alt="logo">
-                        </div>
-                        <div class="mt-4">
-                            <h3 class="text-2xl font-bold">Education</h3>
-                        </div>
+                </div>
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="grid bg-seconder items-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/search.png') }}" class="w-8 md:w-20 lg:w-24" alt="logo">
                     </div>
-                </a>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Search Book</h3>
+                        <h6>You can search book in any other genres or other categories</h6>
+                    </div>
+                </div>
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="grid bg-seconder items-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/chat.png') }}" class="w-8 md:w-20 lg:w-24" alt="logo">
+                    </div>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Review Book</h3>
+                        <h6>You can search book in any other genres or other categories</h6>
+                    </div>
+                </div>
             </div>
         </div>
-        <div id="testimoni" class="w-full h-fit px-14 py-12 bg-white text-black bg-cover bg-center bg-no-repeat">
+        <div id="categories"
+            class="w-full h-fit bg-white text-black bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
             <div class="grid justify-center items-center text-center">
-                <h2 class="text-3xl font-semibold">Testimonial</h2>
-                <h6>This a review about the library from any other customers</h6>
+                <h2 class="text-lg md:text-3xl font-bold">Categories</h2>
+                <h6 class="text-sm md:text-xl lg:text-2xl">We have advantages over other digital libraries</h6>
             </div>
-            <div class="grid grid-cols-4 gap-8 my-8">
-                <div class="border-2 border-seconder rounded-xl py-14">
+            <div class="grid grid-cols-3 gap-4 md:gap-8 mt-4 md:mt-12">
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="griditems-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/comic.png') }}" class="w-16  md:w-20 lg:w-32" alt="logo">
+                    </div>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Comic</h3>
+                    </div>
+                </div>
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="griditems-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/novel.png') }}" class="w-16  md:w-20 lg:w-32" alt="logo">
+                    </div>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Novel</h3>
+                    </div>
+                </div>
+                <div
+                    class="grid rounded-md md:rounded-lg lg:rounded-3xl shadow-md md:shadow-lg justify-center items-center text-center h-50 md:h-90">
+                    <div
+                        class="griditems-center justify-center py-8 md:py-12 rounded-md md:rounded-lg lg:rounded-xl mx-2 md:mx-8 lg:mx-16 mt-4 md:mt-8">
+                        <img src="{{ asset('images/edu.png') }}" class="w-16 md:w-20 lg:w-32"alt="logo">
+                    </div>
+                    <div class="text-center my-4 mx-2">
+                        <h3 class="font-bold">Edukasi</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="testimoni" class="w-full h-fit bg-white text-black bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
+            <div class="grid justify-center items-center text-center">
+                <h2 class="text-lg md:text-3xl font-semibold">Testimonial</h2>
+                <h6 class="text-sm md:text-xl lg:text-2xl">This a review about the library from any other customers</h6>
+            </div>
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-8 my-4 md:my-8">
+                <div class="grid justify-center items-center border-2 border-seconder rounded-md md:rounded-xl py-4 md:py-8 lg:py-14">
                     <div class="flex justify-center items-center">
-                        <img src="{{ asset('images/milea.png') }}" class="w-28" alt="logo">
+                        <img src="{{ asset('images/milea.png') }}" class="w-12 lg:w-28" alt="logo">
                     </div>
-                    <div class="mt-4 text-center px-8">
-                        <h3 class="text-4xl font-bold">Milea</h3>
+                    <div class="mt-4 text-center px-2 lg:px-8">
+                        <h3 class="text-lg md:text-2xl lg:text-4xl font-bold">Milea</h3>
                         <h4 class="text-light text-slate-400">Journalist</h4>
                         <h6>This place has so much collection book, there was a book about void centuries</h6>
                     </div>
                 </div>
-                <div class="border-2 border-seconder rounded-xl py-14">
+                <div class="grid justify-center items-center border-2 border-seconder rounded-md md:rounded-xl py-4 md:py-8 lg:py-14">
                     <div class="flex justify-center items-center">
-                        <img src="{{ asset('images/zoro.png') }}" class="w-28" alt="logo">
+                        <img src="{{ asset('images/zoro.png') }}" class="w-12 lg:w-28" alt="logo">
                     </div>
-                    <div class="mt-4 text-center px-8">
-                        <h3 class="text-4xl font-bold">Zoro</h3>
+                    <div class="mt-4 text-center px-2 lg:px-8">
+                        <h3 class="text-lg md:text-2xl lg:text-4xl font-bold">Zoro</h3>
                         <h4 class="text-light text-slate-400">Swordman</h4>
                         <h6>I don’t know where is it but i think robin would like this place</h6>
                     </div>
                 </div>
-                <div class="border-2 border-seconder rounded-xl py-14">
+                <div class="grid justify-center items-center border-2 border-seconder rounded-md md:rounded-xl py-4 md:py-8 lg:py-14">
                     <div class="flex justify-center items-center">
-                        <img src="{{ asset('images/movis.png') }}" class="w-28" alt="logo">
+                        <img src="{{ asset('images/movis.png') }}" class="w-12 lg:w-28" alt="logo">
                     </div>
-                    <div class="mt-4 text-center px-8">
-                        <h3 class="text-4xl font-bold">Movis</h3>
+                    <div class="mt-4 text-center px-2 lg:px-8">
+                        <h3 class="text-lg md:text-2xl lg:text-4xl font-bold">Movis</h3>
                         <h4 class="text-light text-slate-400">College Student</h4>
-                        <h6>This place very helpful for me to study this place give a feel calmly so nice</h6>
+                        <h6>This place very helpful for me to study this place give a chill vibes</h6>
                     </div>
                 </div>
-                <div class="border-2 border-seconder rounded-xl py-14">
+                <div class="grid justify-center items-center border-2 border-seconder rounded-md md:rounded-xl py-4 md:py-8 lg:py-14">
                     <div class="flex justify-center items-center">
-                        <img src="{{ asset('images/arhan.png') }}" class="w-28" alt="logo">
+                        <img src="{{ asset('images/arhan.png') }}" class="w-12 lg:w-28" alt="logo">
                     </div>
-                    <div class="mt-4 text-center px-8">
-                        <h3 class="text-4xl font-bold">Arhan</h3>
+                    <div class="mt-4 text-center px-2 lg:px-8">
+                        <h3 class="text-lg md:text-2xl lg:text-4xl font-bold">Arhan</h3>
                         <h4 class="text-light text-slate-400">Teacher</h4>
-                        <h6>Mantap Tempatnya😊😁👍</h6>
+                        <h6>Mantap Tempatnya👍😁👍</h6>
                     </div>
                 </div>
             </div>
         </div>
-        <div id="preview" class="w-full h-fit px-14 py-12 bg-white text-black bg-cover bg-center bg-no-repeat">
+        <div id="preview" class="w-full grid justify-center items-center h-fit bg-white text-black bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
             <div class="grid justify-center items-center text-center">
-                <h2 class="text-3xl font-semibold">Preview</h2>
-                <h6>There is preview about this place</h6>
+                <h2 class="text-lg md:text-3xl font-semibold">Preview</h2>
+                <h6 class="text-sm md:text-xl lg:text-2xl">There is preview about this place</h6>
             </div>
-            <div class="w-full rounded-3xl flex justify-center py-8">
-                <img src="{{ asset('images/preview.png') }}" alt="">
+            <div class="rounded-md md:rounded-xl lg:rounded-3xl flex justify-center py-2 md:py-8">
+                <img src="{{ asset('images/preview.png') }}" alt="main preview">
             </div>
-            <div class="w-full flex gap-14 justify-center">
-                <img src="{{ asset('images/preview1.png') }}" alt="">
-                <img src="{{ asset('images/preview2.png') }}" alt="">
-                <img src="{{ asset('images/preview3.png') }}" alt="">
+            <div class="grid grid-cols-3 gap-9 md:gap-12 lg:gap-24 mt-2 md:mt-4">
+                <img src="{{ asset('images/preview1.png') }}" class="w-26 md:w-64 lg:w-80" alt="preview">
+                <img src="{{ asset('images/preview2.png') }}" class="w-26 md:w-64 lg:w-80" alt="preview">
+                <img src="{{ asset('images/preview3.png') }}" class="w-26 md:w-64 lg:w-80" alt="preview">
             </div>
         </div>
-        <div id="location" class="w-full h-fit px-14 py-12 bg-white text-black bg-cover bg-center bg-no-repeat">
+        {{-- <div id="location" class="w-full h-fit bg-white text-black bg-cover bg-center bg-no-repeat px-4 md:10px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
             <div class="grid justify-center items-center text-center">
                 <h2 class="text-3xl font-semibold">Location</h2>
                 <h6>Our place is very easy to reach because there is lots of public transportation</h6>
@@ -166,6 +169,6 @@
                     style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-        </div>
+        </div> --}}
     </div>
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,33 +1,31 @@
-<footer class="w-full bg-seconder flex p-12 items-center font-semibold">
-    <div class="container">
-        <div class="row text-xl font-semibold grid grid-cols-3 gap-4 w-full">
-            <div>
-                <a href="/" class="text-4xl">BookSpark</a>
-            </div>
-            <div class="text-center">
-                <h1>Slogan</h1>
-                <h3>Explroe the sea of Book</h3>
-            </div>
-            <div class="rows-3 text-left">
-                <h1 class="text-center">Contact Us</h1>
-                <div class="flex gap-4">
-                    <h4>Alamat</h4>
-                    <p class="text-sm">Jl. Veteran No.1A, RT.005/RW.002, Babakan, Kec. Tangerang, Kota Tangerang, Banten 15118</p>
-                </div>
-                <div class="flex gap-6 mt-2">
-                    <h4>Social</h4>
-                    <div class="flex gap-4 items-center">
-                        <img src="{{ asset('images/instagram.png') }}" class="h-6" alt="social">
-                        <img src="{{ asset('images/twitter.png') }}" class="h-6" alt="social">
-                        <img src="{{ asset('images/facebook.png') }}" class="h-6" alt="social">
-                    </div>
-                </div>
-            </div>
+<footer
+    class="w-full bg-seconder grid items-center text-left font-semibold px-2 md:7px lg:px-14 py-2 md:py-4 lg:py-6 text-xs md:text-lg lg:text-xl">
+    <div class="row grid">
+        <div class="font-bold text-lg md:text-xl lg:text-2xl">
+            <a href="/">BookSpark</a>
         </div>
-        <div class="row mt-12 mx-64">
-            <div class="flex justify-center items-center border-t-2 border-primary ">
-                <h1>Copyright &copy 2024 BookSpark Made with ❤️ by <a href="https://github.com/N-old">Arnold</a></h1>
+        <div class>
+            <h3 class="font-semibold text-md">Slogan</h3>
+            <h3>Explore the sea of Book</h3>
+        </div>
+        <div class="">
+            <h1 class="mt-2 my-2 text-md font-semibold">Contact Us</h1>
+            <div class="flex mt-2 gap-2">
+                <h4>Alamat</h4>
+                <p class="">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="flex mt-2">
+                <h4>Social</h4>
+                <div class="flex items-center ml-2 gap-2 md:gap-4 lg:gap-8">
+                    <img src="{{ asset('images/instagram.png') }}" class="h-6" alt="social">
+                    <img src="{{ asset('images/twitter.png') }}" class="h-6" alt="social">
+                    <img src="{{ asset('images/facebook.png') }}" class="h-6" alt="social">
+                </div>
             </div>
         </div>
     </div>
+    <div class="flex justify-center items-center border-t-2 border-primary mt-2">
+        <h1 class="mt-2">Copyright &copy 2024 BookSpark Made with ❤️ by <a href="https://github.com/N-old">Arnold</a></h1>
+    </div>
+
 </footer>
